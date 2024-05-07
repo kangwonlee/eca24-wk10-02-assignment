693f7a8521deb3a5a1c882052980279c97a82d1a
Kangwon Lee (Education) <kangwonlee@users.noreply.github.com>
2024-05-07 10:35:23 +0900

    add random delta_x to sample.py

--- sample.py
+++ sample.py
@@ -9,13 +9,17 @@ def sample_main():
     x1_deg = 0
     x2_deg = 360
 
-    x_deg_array = np.arange(x1_deg, x2_deg)
+    delta_x = random.uniform(0.5, 1.5)
+
+    x_deg_array = np.arange(x1_deg, x2_deg, delta_x)
     x_rad_array = np.deg2rad(x_deg_array)
 
     d = main.int_sin(x_rad_array)
 
-    i = random.choice(x_deg_array)
-    print(f"area at x = {i} deg is {d['a_array'][int(i)]}")
+    i = random.randint(0, len(x_deg_array)-1)
+    x_i = x_deg_array[i]
+    rect_i = d['a_array'][i]
+    print(f"area at x = {x_i} deg is {rect_i}")
 
     print(f"numerical integration result is {d['area']}")
 
