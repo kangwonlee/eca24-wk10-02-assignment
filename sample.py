97393b7ae5a06a00e9a3a9aa1c92c1ab98efe92e
KangWon LEE <kangwon@gmail.com>
2024-05-07 21:34:11 +0900

    fix sample.py
    removing exact function

--- sample.py
+++ sample.py
@@ -23,10 +23,8 @@ def sample_main():
 
     print(f"numerical integration result is {result_0['area_0']}")
 
-    exact = main.exact_int_cos(x1_rad, x2_rad)
-
-    print(f"exact integration result is {exact}")
-    print(f"error is {exact - result_0['area_0']}")
+    epsilon = 1e-6
+    print(main.compare_int_cos(x1_rad, x2_rad, n, epsilon))
 
 
 if "__main__" == __name__:
